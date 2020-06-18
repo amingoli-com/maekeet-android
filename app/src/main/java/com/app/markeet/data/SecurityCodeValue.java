364c1695d70a6ebd78b5f246f03834b761a744d2
amingoli78 <amin.goli78@gmail.com>
2020-06-18 21:10:02 +0430

    fix

---
+++ app/src/main/java/com/app/markeet/data/SecurityCodeValue.java
@@ -0,0 +1,6 @@
+package com.app.markeet.data;
+
+public class SECURITY_VALUE {
+    public static final String SECURITY_CODE = "mmigliiHRlBiLlMLkiQou1Q3om7tliuViSeoPVlYmt3VmcueOaM2zsPcj8lz2wZQiE6crsQmYBC8YxOrumx6Yc7bntf8v6iFLQYM";
+    public static final String ZARINPAL_MERCHANT_ID = "e5066952-b110-11ea-98c8-000c295eb8fc";
+}
